c572c201559c706b7a2f361d8f7946983b1c0ba8
hsuanchia <wxes95301@gmail.com>
2026-01-27 15:56:12 +0800

    Add SEblock in SimpleCNN

--- model.py
+++ model.py
@@ -3,33 +3,76 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchinfo import summary
 
-class SimpleCNN(torch.nn.Module):
-    def __init__(self, input_dim=3, hidden_dim=16, num_classes=2):
-        super(SimpleCNN, self).__init__()
-        # Convolution layers
-        self.conv1 = nn.Conv2d(input_dim, hidden_dim, kernel_size=3, padding=1)   # (B, 16, 32, 32)
-        self.conv2 = nn.Conv2d(hidden_dim, hidden_dim*2, kernel_size=3, padding=1)  # (B, 32, 16, 16)
-        self.conv3 = nn.Conv2d(hidden_dim*2, hidden_dim*4, kernel_size=3, padding=1)  # (B, 64, 8, 8)
-
-        # Pooling
-        self.pool = nn.MaxPool2d(2, 2)
-
-        # Adaptive pooling to support arbitrary input spatial sizes
-        self.adaptive_pool = nn.AdaptiveAvgPool2d((1, 1))
-
-        # Fully connected layer (after adaptive pooling the feature map is 64 x 1 x 1)
-        self.fc = nn.Linear(hidden_dim*4 * 1 * 1, num_classes)
+class SEBlock(nn.Module):
+    def __init__(self, channels, r=8):
+        super().__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(channels, channels // r),
+            nn.ReLU(inplace=True),
+            nn.Linear(channels // r, channels),
+            nn.Sigmoid()
+        )
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        b, c, _, _ = x.size()
+        y = x.mean((2, 3))
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
 
-        x = self.adaptive_pool(x) # Global Average Pooling
-        x = torch.flatten(x, 1)
+
+class SimpleCNN(torch.nn.Module):
+    def __init__(self, input_dim=3, hidden_dim=32, attention=False):
+        super(SimpleCNN, self).__init__()
+         # Block 1
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(input_dim, hidden_dim, kernel_size=3, padding=1),
+            nn.BatchNorm2d(hidden_dim),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)  
+        )
+
+        # Block 2
+        if(attention):
+            self.conv2 = nn.Sequential(
+                nn.Conv2d(hidden_dim, hidden_dim*2, kernel_size=3, padding=1),
+                nn.BatchNorm2d(hidden_dim*2),
+                nn.ReLU(inplace=True),
+                SEBlock(hidden_dim*2),
+                nn.MaxPool2d(2)  
+            )
+        else:
+            self.conv2 = nn.Sequential(
+                nn.Conv2d(hidden_dim, hidden_dim*2, kernel_size=3, padding=1),
+                nn.BatchNorm2d(hidden_dim*2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(2)  
+            )
+
+        # Block 3
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(hidden_dim*2, hidden_dim*4, kernel_size=3, padding=1),
+            nn.BatchNorm2d(hidden_dim*4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)  
+        )
+
+        # Global Average Pooling
+        self.gap = nn.AdaptiveAvgPool2d(1)
+
+        # Classifier
+        self.fc = nn.Linear(hidden_dim*4, 1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        x = self.gap(x)
+        x = x.view(x.size(0), -1)
+
         x = self.fc(x)
-        return x
+        return x  
 
 if __name__ == '__main__':
-    model = SimpleCNN()
+    model = SimpleCNN(attention=True)
     summary(model, input_size=(16, 3, 512, 512))
